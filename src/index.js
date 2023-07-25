1a33d136bda2360e8d05a89fa159266b7430c3c5
Kinjal Jain <jainkinjal021196@gmail.com>
2023-07-25 15:32:47 +0530

    GraphQL  query added

--- src/index.js
+++ src/index.js
@@ -6,24 +6,35 @@ import './index.scss';
 import { Provider } from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
 import App from './App';
+import { ApolloClient , InMemoryCache, ApolloProvider } from '@apollo/client';
 
 import { store ,persistor} from './store/store';
 import reportWebVitals from './reportWebVitals';
 
 import { Elements } from '@stripe/react-stripe-js';
 import { stripePromise } from './utils/stripe/stripe.utils';
+
+const client = new ApolloClient({
+  uri: 'https://crwn-clothing.com/', //graphql server url --playground
+  cache: new InMemoryCache()
+});
+
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter>
-          <Elements stripe={ stripePromise }>
-            <App />
-          </Elements>
-        </BrowserRouter>
-      </PersistGate>
-    </Provider>
+    <ApolloProvider client={client}>
+        <Provider store={store}>
+          <PersistGate loading={null} persistor={persistor}>
+            <BrowserRouter>
+              <Elements stripe={ stripePromise }>
+                <App />
+              </Elements>
+            </BrowserRouter>
+          </PersistGate>
+        </Provider>
+    </ApolloProvider>
   </React.StrictMode>
 );
 
